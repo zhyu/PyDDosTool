2646d5587dab69487a8e2a7e002a923ee6191f27
angellwings <angellwings@gmail.com>
2012-10-22 22:36:58 +0800

    move functions to Func.py.

--- MainWindow.py
+++ MainWindow.py
@@ -4,33 +4,39 @@
 Module implementing MainWindow.
 """
 
-import socket
-from PySide import QtCore, QtGui
+from PySide.QtCore import Slot
+from PySide.QtGui import QMainWindow, QDesktopWidget
 from Ui_MainWindow import Ui_MainWindow
+import Func
 
 
-class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     """
-    Class documentation goes here.
+    Class of main window.
     """
     def __init__(self, parent=None):
         """
-        Constructor of main window
+        Constructor of main window.
         """
-        QtGui.QMainWindow.__init__(self, parent)
+        QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.center()
-    
-    def center(self):
-        screen = QtGui.QDesktopWidget().screenGeometry()
+        
+    def center(self, parent=None):
+        screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-        
-    @QtCore.Slot()
+    
+    @Slot()
     def on_getIPBtn_clicked(self):
-        """
-        get ip address by domain name, using socket
-        """
         domainName = self.domainInput.text()
-        ipAddress = socket.gethostbyname(domainName)
-        self.ipInput.setText(ipAddress)
+        ipaddress = Func.getIP(domainName)
+        self.ipInput.setText(ipaddress)
+    
+    @Slot()
+    def on_atkBtn_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
--- Ui_MainWindow.py
+++ Ui_MainWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/zhyu/workspace/PyDDosTool/MainWindow.ui'
 #
-# Created: Thu Oct 18 11:25:05 2012
+# Created: Mon Oct 22 22:15:55 2012
 #      by: pyside-uic 0.2.13 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -16,13 +16,13 @@ class Ui_MainWindow(object):
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.domainInput = QtGui.QLineEdit(self.centralWidget)
-        self.domainInput.setGeometry(QtCore.QRect(90, 30, 131, 31))
+        self.domainInput.setGeometry(QtCore.QRect(90, 10, 131, 31))
         self.domainInput.setObjectName("domainInput")
         self.ipInput = QtGui.QLineEdit(self.centralWidget)
-        self.ipInput.setGeometry(QtCore.QRect(90, 70, 131, 31))
+        self.ipInput.setGeometry(QtCore.QRect(90, 50, 131, 31))
         self.ipInput.setObjectName("ipInput")
         self.portInput = QtGui.QLineEdit(self.centralWidget)
-        self.portInput.setGeometry(QtCore.QRect(90, 110, 131, 31))
+        self.portInput.setGeometry(QtCore.QRect(90, 90, 131, 31))
         self.portInput.setObjectName("portInput")
         self.btnGrp = QtGui.QGroupBox(self.centralWidget)
         self.btnGrp.setGeometry(QtCore.QRect(240, 10, 120, 151))
@@ -41,26 +41,33 @@ class Ui_MainWindow(object):
         self.tcpChk.setGeometry(QtCore.QRect(10, 60, 94, 21))
         self.tcpChk.setObjectName("tcpChk")
         self.getIPBtn = QtGui.QPushButton(self.centralWidget)
-        self.getIPBtn.setGeometry(QtCore.QRect(20, 170, 91, 23))
+        self.getIPBtn.setGeometry(QtCore.QRect(30, 170, 91, 23))
         self.getIPBtn.setObjectName("getIPBtn")
-        self.AtkBtn = QtGui.QPushButton(self.centralWidget)
-        self.AtkBtn.setGeometry(QtCore.QRect(130, 170, 91, 23))
-        self.AtkBtn.setObjectName("AtkBtn")
-        self.StopBtn = QtGui.QPushButton(self.centralWidget)
-        self.StopBtn.setGeometry(QtCore.QRect(240, 170, 91, 23))
-        self.StopBtn.setObjectName("StopBtn")
+        self.atkBtn = QtGui.QPushButton(self.centralWidget)
+        self.atkBtn.setGeometry(QtCore.QRect(150, 170, 91, 23))
+        self.atkBtn.setObjectName("atkBtn")
+        self.quitBtn = QtGui.QPushButton(self.centralWidget)
+        self.quitBtn.setGeometry(QtCore.QRect(270, 170, 91, 23))
+        self.quitBtn.setObjectName("quitBtn")
         self.lb_1 = QtGui.QLabel(self.centralWidget)
-        self.lb_1.setGeometry(QtCore.QRect(30, 40, 52, 14))
+        self.lb_1.setGeometry(QtCore.QRect(30, 20, 52, 14))
         self.lb_1.setObjectName("lb_1")
         self.lb_2 = QtGui.QLabel(self.centralWidget)
-        self.lb_2.setGeometry(QtCore.QRect(30, 80, 52, 14))
+        self.lb_2.setGeometry(QtCore.QRect(30, 60, 52, 14))
         self.lb_2.setObjectName("lb_2")
         self.lb_3 = QtGui.QLabel(self.centralWidget)
-        self.lb_3.setGeometry(QtCore.QRect(30, 120, 52, 14))
+        self.lb_3.setGeometry(QtCore.QRect(30, 100, 52, 14))
         self.lb_3.setObjectName("lb_3")
+        self.countInput = QtGui.QLineEdit(self.centralWidget)
+        self.countInput.setGeometry(QtCore.QRect(90, 130, 131, 31))
+        self.countInput.setObjectName("countInput")
+        self.lb_4 = QtGui.QLabel(self.centralWidget)
+        self.lb_4.setGeometry(QtCore.QRect(30, 140, 52, 14))
+        self.lb_4.setObjectName("lb_4")
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.quitBtn, QtCore.SIGNAL("clicked()"), MainWindow.close)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -74,11 +81,13 @@ class Ui_MainWindow(object):
         self.udpChk.setText(QtGui.QApplication.translate("MainWindow", "UDP Flood", None, QtGui.QApplication.UnicodeUTF8))
         self.tcpChk.setText(QtGui.QApplication.translate("MainWindow", "TCP Flood", None, QtGui.QApplication.UnicodeUTF8))
         self.getIPBtn.setText(QtGui.QApplication.translate("MainWindow", "获取IP", None, QtGui.QApplication.UnicodeUTF8))
-        self.AtkBtn.setText(QtGui.QApplication.translate("MainWindow", "攻击", None, QtGui.QApplication.UnicodeUTF8))
-        self.StopBtn.setText(QtGui.QApplication.translate("MainWindow", "停止", None, QtGui.QApplication.UnicodeUTF8))
+        self.atkBtn.setText(QtGui.QApplication.translate("MainWindow", "攻击", None, QtGui.QApplication.UnicodeUTF8))
+        self.quitBtn.setText(QtGui.QApplication.translate("MainWindow", "退出", None, QtGui.QApplication.UnicodeUTF8))
+        self.quitBtn.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+Q", None, QtGui.QApplication.UnicodeUTF8))
         self.lb_1.setText(QtGui.QApplication.translate("MainWindow", "目标域名:", None, QtGui.QApplication.UnicodeUTF8))
         self.lb_2.setText(QtGui.QApplication.translate("MainWindow", "目标IP:", None, QtGui.QApplication.UnicodeUTF8))
         self.lb_3.setText(QtGui.QApplication.translate("MainWindow", "目标端口:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lb_4.setText(QtGui.QApplication.translate("MainWindow", "攻击次数:", None, QtGui.QApplication.UnicodeUTF8))
 
 
 if __name__ == "__main__":
